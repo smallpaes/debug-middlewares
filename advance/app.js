244a4fa0204561332580a5eef3155a7bea85f388
Mike <mike.huang.mikank@gmail.com>
2019-07-24 16:45:06 +0800

    Add result print out to every route

--- advance/app.js
+++ advance/app.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 const port = 3000
-const { getCurrentTime, getTimeDifference } = require('./getTime')
+const getData = require('./getData')
 
 // debug middleware: show time-stamps
 app.use((req, res, then) => {
@@ -12,29 +12,30 @@ app.use((req, res, then) => {
 
 // 列出全部 Todo
 app.get('/', (req, res) => {
+  console.log(getData(req.requestTime, req.method, req.url))
   res.send('列出全部 Todo')
 })
 
 // 新增一筆 Todo 頁面
 app.get('/new', (req, res) => {
-  const currentTime = new Date()
-  const requestTimeStamps = getCurrentTime(req.requestTime)
-  const timeDifference = getTimeDifference(req.requestTime, currentTime)
-  console.log(`${requestTimeStamps} | ${req.method} from ${req.url} | total time: ${timeDifference}ms`)
+  console.log(getData(req.requestTime, req.method, req.url))
   res.send('新增 Todo 頁面')
 })
 
 // 顯示一筆 Todo 的詳細內容
 app.get('/:id', (req, res) => {
+  console.log(getData(req.requestTime, req.method, req.url))
   res.send('顯示一筆 Todo')
 })
 
 // 新增一筆  Todo
 app.post('/', (req, res) => {
+  console.log(getData(req.requestTime, req.method, req.url))
   res.send('新增一筆  Todo')
 })
 
 app.delete('/:id/delete', (req, res) => {
+  console.log(getData(req.requestTime, req.method, req.url))
   res.send('刪除 Todo')
 })
 
